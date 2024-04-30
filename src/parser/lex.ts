88596c5b82819ea2a7884a2ca6c4322a19d8d06a
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2024-04-30 07:23:10 +0000

    rewrite lexer/parser

--- src/parser/lex.ts
+++ src/parser/lex.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2020 Sieve
+ * Copyright (C) 2024 Sieve
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -14,128 +14,126 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  **/
-import {Location} from './ast.ts';
-import {CharStream, cs_peek, cs_next, cs_loc} from './cs.ts';
+import {CharacterStream, TokenStream} from "@sieve/nonstd/data";
+type TokenType = "DIRECTIVE" | "DATE" | "NUMBER" | "ACCOUNT" | "STRING";
 
-export const DIRECTIVE = "DIRECTIVE";
-export const DATE = "DATE";
-export const NUMBER = "NUMBER";
-export const STRING = "STRING";
-export const TEXT = "TEXT";
-export const SYM = "SYM";
-
-export interface Token {
-	loc: Location,
-	type: string,
-	lexeme: string,
+interface Token {
+    type: TokenType;
+    index: number;
+    lexeme: string;
 }
 
-export function skip_comment(cs: CharStream, x?: string) {
-	x = x || cs_peek(cs);
-	if (x === ';') {
-		// skip comment
-		while (!cs.eof) {
-			const x = cs_next(cs);
-			if (x === '\n') break;
-		}
-		cs.index--;
-	}
+function token(cs: CharacterStream, type: TokenType, start: number, end?: number) {
+    end = end || cs.get_index();
+    return { type: type, index: start, lexeme: cs.substring(start, end)};
 }
 
-export function skip_ws(cs: CharStream) {
-	while (!cs.eof) {
-		const x = cs_peek(cs);
-		if (x === ' ' || x === '\n') {
-			cs_next(cs);
-		}
-		else if (x === ';') {
-			skip_comment(cs, x);
-		}
-		else {
-			break;
-		}
-	}
+function skip_comment(cs: CharacterStream) {
+    while (!cs.eof()) {
+        cs.next();
+        if (cs.peek() === '\n') break;
+    }
 }
 
-function build_token(cs: CharStream, type: string, start: number, end?: number) {
-	end = end || cs.index;
-	return { loc: cs_loc(cs, start), lexeme: cs.x.substring(start, end), type: type};
-}
-export function read_date(cs: CharStream): Token {
-	skip_ws(cs);
-	const index = cs.index;
-
-	const read_digit = (x: string) => { if (x >= '0' && x <= '9') {return x; } else { throw new Error(x) } }
-	const read_sep = (x: string) => { if (x === '-' || x === '/') {return x; } else { throw new Error(x) } }
-
-	let n = 0;
-	while (!cs.eof && n < 10) {
-		const x = cs_next(cs);
-		if (n < 4) {
-			read_digit(x);
-		}
-		else if (n === 4) {
-			read_sep(x)
-		}
-		else if (n < 7) {
-			read_digit(x);
-		}
-		else if (n === 7) {
-			read_sep(x)
-		}
-		else if (n < 10) {
-			read_digit(x);
-		}
-		n += 1;
-	}
-	return build_token(cs, DATE, index);
+function lex_error(cs: CharacterStream, n: number, x: string) {
+    const [line,character] = cs.get_line_and_character(n);
+    const y = cs.substring(n, cs.get_index()).trim();
+    return new Error(`"${y}" : ${x} (${cs.name}:${line}:${character})`);
 }
 
-export function read_number(cs: CharStream): Token {
-	skip_ws(cs);
-	const index = cs.index;
-	cs_next(cs); // skip -ve sign or 0-9 or DOT or COMMA
-	while (!cs.eof) {
-		const x = cs_peek(cs);
-		if ((x >= '0' && x <= '9') || x === '.' || x === ',') {
-			cs_next(cs);
-		}
-		else {
-			break;
-		}
-	}
-	return build_token(cs, NUMBER, index);
+function read_date_or_number(cs: CharacterStream): Token {
+    const n = cs.get_index();
+    cs.next(); // skip -ve sign or 0-9 or DOT or COMMA
+    while (!cs.eof()) {
+        const x = cs.peek();
+        if ((x >= '0' && x <= '9') || x === '.' || x === ',' || x === '-') {
+            cs.next();
+        }
+        else {
+            break;
+        }
+    }
+    const x = token(cs, "NUMBER", n);
+    if (/^\d{4}-\d{2}-\d{2}$/.test(x.lexeme)) {
+        x.type = "DATE";
+    }
+    else if (/^-?[\d,]+(?:\.\d{2})?$/.test(x.lexeme)) {
+        x.type = "NUMBER";
+    }
+    else {
+        throw lex_error(cs, n, "NOT A DATE/NUMBER");
+    }
+    return x;
 }
 
-export function read_directive(cs: CharStream): Token {
-	skip_ws(cs);
-	const index = cs.index;
-	while (!cs.eof) {
-		const x = cs_peek(cs);
-		if (!(x === '-' || (x >= 'a' && x <= 'z'))) break;
-		cs_next(cs);
-	}
-	return build_token(cs, DIRECTIVE, index);
+function read_directive(cs: CharacterStream): Token {
+    const n = cs.get_index();
+    while (!cs.eof()) {
+        const x = cs.peek();
+        if (x === ' ' || (x >= 'a' && x <= 'z')) {
+            cs.next();
+        }
+        else {
+            break;
+        }
+    }
+    const x = token(cs, "DIRECTIVE", n);
+    x.lexeme = x.lexeme.trim();
+    return x;
 }
 
-export function read_string(cs: CharStream): Token {
-	skip_ws(cs);
-	if (cs_next(cs) !== '"') throw new Error();
-	const index = cs.index;
-	while (!cs.eof) {
-		const x = cs_next(cs);
-		if (x === '"') break;
-	}
-	return build_token(cs, STRING, index, cs.index-1);
+function read_account(cs: CharacterStream): Token {
+    const n = cs.get_index();
+    while (!cs.eof()) {
+        const x = cs.peek();
+        if (x === ';' || x === '\t' || x === '\n' || x === ' ') break;
+        cs.next();
+    }
+    return token(cs, "ACCOUNT", n);
 }
 
-export function read_text(cs: CharStream): Token {
-	skip_ws(cs);
-	const index = cs.index;
-	while (!cs.eof) {
-		const x = cs_peek(cs);
-		if (x === ';' || x === '\t' || x === '\n' || x === ' ') break;
-		cs_next(cs);
-	}
-	return build_token(cs, TEXT, index);
+function read_string(cs: CharacterStream): Token {
+    const n = cs.get_index();
+    cs.next();
+    while (!cs.eof()) {
+        const x = cs.next();
+        if (x === '"') break;
+    }
+    const a =  token(cs, "STRING", n);
+    a.lexeme = a.lexeme.substring(1, a.lexeme.length-1);
+    return a;
+}
+
+export {TokenStream, type Token};
+
+export function lex(file: string, x: string): TokenStream<Token> {
+    const cs = new CharacterStream(x.replace(/\r\n?/g, "\n"), file);
+
+    const xs: Token[] = [];
+    while (true) {
+        cs.skip_ws();
+        if (cs.eof()) break;
+        const n = cs.get_index();
+        const x = cs.peek();
+
+        if (x === ';') {
+            skip_comment(cs);
+        }
+        else if (x === '-' || (x >= '0' && x <= '9')) {
+            xs.push(read_date_or_number(cs));
+        }
+        else if (x >= 'a' && x <= 'z') {
+            xs.push(read_directive(cs));
+        }
+        else if (x >= 'A' && x <= 'Z') {
+            xs.push(read_account(cs));
+        }
+        else if (x >= '"') {
+            xs.push(read_string(cs));
+        }
+        else {
+            throw lex_error(cs, n, "INVALID CHARACTER: "+x);
+        }
+    }
+    return new TokenStream(xs, file);
 }
--- src/parser/parse.ts
+++ src/parser/parse.ts
@@ -14,200 +14,193 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  **/
-import {cur_s2n, log_info, read_text_file} from "../common.ts";
+import {cur_s2n, log_info} from "../common.ts";
+import {fs_read_utf8 } from "@sieve/nonstd/os";
+
 import {
-	Date,
-	OpenLedger,
-	Ledger,
-	NODE_DATE,
-	NODE_OPEN_LEDGER,
-	NODE_INCLUDE_FILE,
-	NODE_INCLUDE_JOURNAL,
-	IncludeFile,
-	IncludeJournal,
-	UseAccount,
-	NODE_USE_ACCOUNT,
-	Entry,
-	JournalEntry, build_accounts, resolve_amount_type, entry,
-} from "./ast.ts";
-import {CharStream, cs_peek, cs_next, cs_loc, cs_new} from './cs.ts';
-import {read_date, read_directive, read_number, read_string, read_text, skip_comment, skip_ws} from "./lex.ts";
+    Ledger,
+    Entry,
+    JournalEntry, build_accounts, resolve_amount_type, entry, OpenLedger,
+} from "../parser/ast.ts";
+import {CharacterStream} from "@sieve/nonstd/data";
+import {TokenStream, Token, lex} from "./lex.ts";
 
-function parse_date(cs: CharStream): Date {
-	const x = read_date(cs);
-	return {
-		type: NODE_DATE,
-		loc: x.loc,
-		date: x.lexeme,
-	};
+interface ParseError extends Error {
+    t: Token;
 }
 
-function parse_entries(cs: CharStream): Entry[] {
-	const xs: Entry[] = [];
-	while (!cs.eof) {
-		const a = cur_s2n(read_number(cs).lexeme);
-		const b = read_text(cs).lexeme;
-		const amount_type = resolve_amount_type(b);
-		xs.push(entry(b, a, amount_type));
-
-		skip_comment(cs);
-		if (cs_peek(cs) === '\n') {
-			cs_next(cs);
-			if (cs_peek(cs) === '\n') {
-				cs_next(cs);
-				break;
-			}
-		}
-	}
-	return xs;
-}
-
-function parse_open_ledger(cs: CharStream): OpenLedger {
-	const loc = cs_loc(cs, cs.index);
-	const xs = parse_entries(cs);
-	return {type: NODE_OPEN_LEDGER, loc: loc, xs: xs};
-}
-
-function parse_use_account(cs: CharStream): UseAccount {
-	const x = read_text(cs)
-	return {type: NODE_USE_ACCOUNT, loc: x.loc, account: x.lexeme};
-}
-
-function parse_directive(cs: CharStream) {
-	const x = read_directive(cs);
-	switch (x.lexeme) {
-		case 'open': {
-			const x = read_directive(cs);
-			switch (x.lexeme) {
-				case 'ledger': return parse_open_ledger(cs);
-				default: throw new Error(x.lexeme);
-			}
-		}
-		case 'use': {
-			const x = read_directive(cs);
-			switch (x.lexeme) {
-				case 'account': return parse_use_account(cs);
-				default: throw new Error(x.lexeme);
-			}
-		}
-		case 'include': {
-			const x = read_string(cs);
-			return {
-				type: NODE_INCLUDE_FILE,
-				loc: x.loc,
-				file: x.lexeme,
-			};
-		}
-		case 'journal': {
-			const x = read_string(cs);
-			return {
-				type: NODE_INCLUDE_JOURNAL,
-				loc: x.loc,
-				file: x.lexeme,
-			};
-		}
-		default: throw new Error(x.lexeme);
-	}
-}
-
-interface CurrentState {
-	date: string,
-	account: string,
-}
-
-function ledger_unposted_last(lg: Ledger) {
-	return lg.unposted[lg.unposted.length-1];
-}
-
-function parse_journal_entry(xs: Entry[], lg: Ledger, current: CurrentState) {
-	const add_je = (x: JournalEntry) => {
-		const last = ledger_unposted_last(lg);
-		if (last && last.date > current.date) throw new Error(`Postings not in sequence: ${last.date} > ${current.date}}`);
-		lg.unposted.push(x);
-	};
-
-	if (current.account) {
-		xs.forEach(e => add_je({date: current.date, xs: [e, entry(current.account, e.amount, e.amount_type === "D" ? "C" : "D")]}));
-	}
-	else {
-		add_je({date: current.date, xs: xs});
-	}
-	current.date = '';
+function parse_open_ledger(ts: TokenStream<Token>) {
+    const xs = parse_entries(ts);
+    return {xs: xs};
 }
 
 function resolve_path(old_file: string, new_file: string) {
-	let x = old_file;
-	let n = x.lastIndexOf('/');
-	if (n == -1) return new_file;
-	return x.substring(0, n+1)+new_file;
+    const x = old_file;
+    const n = x.lastIndexOf('/');
+    if (n == -1) return new_file;
+    return x.substring(0, n+1)+new_file;
+}
+
+function build_error_string(source: string, t: Token, file: string, x: string) {
+    const cs = new CharacterStream(source.replace(/\r\n?/g, "\n"), file);
+    const [line,character] = cs.get_line_and_character(t.index);
+    const y = cs.substring(t.index, t.index+20).trim();
+    return `"${y}" : ${x} (${cs.name}:${line}:${character})`;
+}
+
+function parse_error(t: Token) {
+    throw {
+        t: t,
+    } as ParseError;
+}
+
+function parse_directive(ts: TokenStream<Token>, lg: Ledger, file: string, current: CurrentState) {
+    const x = ts.next();
+    switch (x.lexeme) {
+        case 'open ledger': {
+            const y= parse_open_ledger(ts);
+            build_accounts(y as OpenLedger, lg);
+            break;
+        }
+        case 'use account': {
+            const y = ts.next();
+            if (y.type !== "ACCOUNT") throw parse_error(y);
+            current.account = y.lexeme;
+            break;
+        }
+        case 'include': {
+            const y = ts.next();
+            if (y.type !== "STRING") throw parse_error(y);
+            do_parse(resolve_path(file, y.lexeme), lg);
+            break;
+        }
+        case 'journal': {
+            const lgx = {
+                start_date: lg.start_date,
+                end_date: lg.end_date,
+                accounts: lg.accounts,
+                unposted: [],
+            };
+
+            const y = ts.next();
+            if (y.type !== "STRING") throw parse_error(y);
+            do_parse(resolve_path(file, y.lexeme), lgx);
+            lg.unposted = lg.unposted.concat(lgx.unposted);
+            break;
+        }
+        default: throw parse_error(x);
+    }
+}
+
+interface CurrentState {
+    date: string,
+    account: string,
+}
+
+function ledger_unposted_last(lg: Ledger) {
+    return lg.unposted[lg.unposted.length-1];
+}
+
+function parse_entries(ts: TokenStream<Token>): Entry[] {
+    const xs: Entry[] = [];
+    while (!ts.eof()) {
+        const aa = ts.next();
+        if (aa.type !== "NUMBER") parse_error(aa);
+
+        const bb = ts.next();
+        if (bb.type !== "ACCOUNT") parse_error(bb);
+
+        const a = cur_s2n(aa.lexeme);
+        const b = bb.lexeme;
+        const amount_type = resolve_amount_type(b);
+        xs.push(entry(b, a, amount_type));
+
+        if (ts.eof() || ts.peek().type !== "NUMBER") {
+            break;
+        }
+    }
+    return xs;
+}
+
+function parse_journal_entry(ts: TokenStream<Token>, lg: Ledger, current: CurrentState) {
+    const xs = parse_entries(ts);
+
+    const add_je = (x: JournalEntry) => {
+        const last = ledger_unposted_last(lg);
+        if (last && last.date > current.date) throw new Error(`Postings not in sequence: ${last.date} > ${current.date}}`);
+        lg.unposted.push(x);
+    };
+
+    if (current.account) {
+        xs.forEach(e => add_je({date: current.date, xs: [e, entry(current.account, e.amount, e.amount_type === "D" ? "C" : "D")]}));
+    }
+    else {
+        add_je({date: current.date, xs: xs});
+    }
+    current.date = '';
 }
 
 function do_parse(file: string, lg: Ledger) {
-	log_info(`Parsing: ${file}`);
-	const cs = cs_new(read_text_file(file));
+    log_info(`Parsing: ${file}`);
+    const source = fs_read_utf8(file);
+    const ts= lex(file, source);
+    try {
+        _do_parse(ts, file, lg);
+    }
+    catch (e) {
+        if (e.t) {
+            const f = new Error(build_error_string(source, e.t, file, "@"));
+            f.cause = e.cause;
+            throw f;
+        }
+        else {
+            throw e;
+        }
+    }
+}
 
-	let current = {
-		date: '',
-		account: '',
-	};
+function _do_parse(ts: TokenStream<Token>, file: string, lg: Ledger) {
+    const current = {
+        date: '',
+        account: '',
+    };
 
-	while (!cs.eof) {
-		const x = cs_peek(cs);
-		if (x === ';' || x === '\n') {
-			skip_ws(cs);
-		}
-		else if (x === '-') {
-			parse_journal_entry(parse_entries(cs), lg, current);
-		}
-		else if ((x >= '0' && x <= '9')) {
-			if (current.date) {
-				parse_journal_entry(parse_entries(cs), lg, current);
-			}
-			else {
-				const y = parse_date(cs);
-				if (!lg.start_date) lg.start_date = y.date;
-				current.date = y.date;
-			}
-		}
-		else if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')) {
-			const y = parse_directive(cs);
-			switch (y.type) {
-				case NODE_OPEN_LEDGER: build_accounts(y as OpenLedger, lg); break;
-				case NODE_USE_ACCOUNT: current.account = (y as UseAccount).account; break;
-				case NODE_INCLUDE_JOURNAL: {
-					const lgx = {
-						start_date: lg.start_date,
-						end_date: lg.end_date,
-						accounts: lg.accounts,
-						unposted: [],
-					};
-					do_parse(resolve_path(file, (y as IncludeJournal).file), lgx);
-					lg.unposted = lg.unposted.concat(lgx.unposted);
-					break;
-				}
-				case NODE_INCLUDE_FILE: {
-					do_parse(resolve_path(file, (y as IncludeFile).file), lg);
-					break;
-				}
-				default: throw new Error(`type: ${y.type}`);
-			}
-		}
-		else {
-			throw new Error(`#${x}@`);
-		}
-	}
+    while (!ts.eof()) {
+        const x = ts.peek();
+        if (x.type === "DATE") {
+            if (!lg.start_date) lg.start_date = x.lexeme;
+            current.date = x.lexeme;
+            ts.next();
+        }
+        else if (x.type === "NUMBER") {
+            if (current.date) {
+                parse_journal_entry(ts, lg, current);
+            }
+            else {
+                throw parse_error(x);
+            }
+        }
+        else if (x.type === "DIRECTIVE") {
+            parse_directive(ts, lg, file, current);
+        }
+        else {
+            throw parse_error(x);
+        }
+    }
 }
 
 export function parse(file: string): Ledger {
-	const lg: Ledger = {
-		start_date: '',
-		end_date: '',
-		accounts: [],
-		unposted: [],
-	};
-	do_parse(file, lg);
-	lg.unposted = lg.unposted.sort((a, b) => a.date <= b.date ? -1 : 1);
-	const last = lg.unposted[lg.unposted.length-1];
-	lg.end_date = last ? last.date : lg.end_date;
-	return lg;
+    const lg: Ledger = {
+        start_date: '',
+        end_date: '',
+        accounts: [],
+        unposted: [],
+    };
+
+    do_parse(file , lg);
+
+    lg.unposted = lg.unposted.sort((a, b) => a.date <= b.date ? -1 : 1);
+    const last = lg.unposted[lg.unposted.length-1];
+    lg.end_date = last ? last.date : lg.end_date;
+    return lg;
 }
